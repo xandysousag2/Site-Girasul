c26581deaf7657885161ab5a81528d4fbcf3c098
xandysousag2 <xandysousag2@gmail.com>
2026-03-09 14:32:05 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Atualiza ano automaticamente no footer
+document.addEventListener('DOMContentLoaded', () => {
+  const yearSpan = document.getElementById('year');
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
